8d41f3b1cbde3318111d97b505a831b95466c3e7
spider <m15828115523@163.com>
2024-09-07 23:43:34 +0800

    背景: 整合宿主机应用，整合spider-ai 新增包的问题

---
+++ sdk/host-plugin-center-sdk/src/main/java/cn/spider/node/host/plugin/center/sdk/interfaces/package-info.java
@@ -0,0 +1,6 @@
+@ModuleGen(groupPackage = "cn.spider.node.host.plugin.center.sdk.interfaces", name = "services", useFutures = true)
+
+
+package cn.spider.node.host.plugin.center.sdk.interfaces;
+
+import io.vertx.codegen.annotations.ModuleGen;
